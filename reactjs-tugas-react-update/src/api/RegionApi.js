eaa10a2c8b11c0008e12ef1639599d1df9b1717a
sendraabd <sendraabdulfatah@gmail.com>
2023-07-11 12:55:38 +0700

    first-commit

--- reactjs-tugas-react-update/src/api/RegionApi.js
+++ reactjs-tugas-react-update/src/api/RegionApi.js
@@ -1,44 +1,42 @@
 import axios from "axios";
 import config from "../config/config";
 
-const list = async () => {
-  try {
-    const result = await axios.get(`${config.domain}/region`);
-    return result.data;
-  } catch (error) {
-    return await error.message;
-  }
+const regionApi = {
+  list: async () => {
+    try {
+      const result = await axios.get(`${config.domain}/region`);
+      return result.data;
+    } catch (error) {
+      return error.message;
+    }
+  },
+
+  deleted: async (id) => {
+    try {
+      const result = await axios.delete(`${config.domain}/region/${id}`);
+      return result;
+    } catch (error) {
+      return error.message;
+    }
+  },
+
+  create: async (payload) => {
+    try {
+      const result = await axios.post(`${config.domain}/region`, payload);
+      return result;
+    } catch (error) {
+      return error.message;
+    }
+  },
+
+  updated: async (payload, id) => {
+    try {
+      const result = await axios.put(`${config.domain}/region/${id}`, payload);
+      return result;
+    } catch (error) {
+      return error.message;
+    }
+  },
 };
 
-const deleted = async (id) => {
-  try {
-    const result = await axios.delete(`${config.domain}/region/${id}`);
-    return result;
-  } catch (error) {
-    return await error.message;
-  }
-};
-
-const create = async (payload) => {
-  try {
-    const result = await axios.post(`${config.domain}/region`, payload);
-    return result;
-  } catch (error) {
-    return await error.message;
-  }
-};
-
-const updated = async (id, updatedData) => {
-  try {
-    const response = await axios.put(
-      `${config.domain}/region/${id}`,
-      updatedData
-    );
-    return response.data;
-  } catch (error) {
-    console.log("Error updating region data: ", error);
-    throw error;
-  }
-};
-
-export default { list, deleted, create, updated };
+export default regionApi;
